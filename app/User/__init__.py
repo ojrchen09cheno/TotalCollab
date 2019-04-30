12b7aa6d709435869280040ed18ab4dd3e150364
Christopher-Polloni <christopher.polloni16@stjohns.edu>
2019-04-30 10:09:39 -0400

    Delete User Folder

--- app/User/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('User', __name__)
-
-from app.User import forms, routes
--- app/User/routes.py
+++
@@ -1,9 +0,0 @@
-from app.User import bp
-from app.models import *
-from app import db
-from flask import render_template, flash, redirect
-
-
-@bp.route('/')
-def index():
-    return render_template('index.html', title='Welcome')
